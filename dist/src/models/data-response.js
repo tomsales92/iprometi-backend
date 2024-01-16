d77700a19c7e8454006ae1cafeb98043d4442d44
tomsales92 <everton.goncalves92@gmail.com>
2024-01-15 23:09:35 -0300

    ajuste vercel 16

--- dist/src/models/data-response.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DataResponse = void 0;
-class DataResponse {
-}
-exports.DataResponse = DataResponse;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response, response } from 'express';
-import { DataResponse } from './models/data-response';
+import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 
 app.use(cors());
 app.use(express.json());
